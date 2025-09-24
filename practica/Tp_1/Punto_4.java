1ed2395b648067d00d87551d69e1b44ace354941
martin perassi <kerberosclothes@gmail.com>
2025-09-24 06:59:38 +0000

    ejercicio 4 tp1

---
+++ practica/Tp_1/Punto_4.java
@@ -0,0 +1,22 @@
+/*
+ * 4. Dado tres números, escribir el mayor de ellos. Asumir que los tres números son distintos. 
+ */
+
+package Tp_1;
+import java.util.Scanner;
+
+public class Punto_4 {
+
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        int num1, num2, num3;
+
+        System.out.print("ingrese un numero:");
+        System.out.println();
+        num1 = scanner.nextInt();
+        System.out.println("ingrese un numero:");
+        num2 = scanner.nextInt();
+        System.out.println("ingrese un numero:");
+        num3 = scanner.nextInt();
+    }
+}
