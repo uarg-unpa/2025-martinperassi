19c2b2ec6c413d55eefdc5d4caa8a93734cca564
martinperassi43 <martinperassi4333@gmail.com>
2025-10-11 13:27:00 -0300

    apuntes metodo toString

---
+++ practica/Apuntes/ToString.java
@@ -0,0 +1,29 @@
+package Apuntes;
+
+public class ToString {
+    //ATRIBUTOS
+    private String nombre;
+    private int numero;
+
+    //CONSTRUCTOR
+    ToString(String nombre, int numero){
+        this.nombre = nombre;
+        this.numero = numero;
+    }
+
+
+    //toString()
+    
+    @Override
+    public String toString(){
+        return "[Nombre: " + nombre + ", Numero: " + numero + "]";
+    }
+
+
+    public static void main(String[] args) {
+        ToString p1 = new ToString("Martin", 12);
+
+        System.out.println(p1);
+        
+    }
+}
