9bf85e17065e6c3b85bd5ec45f2cebd037a7bd4c
martinperassi43 <martinperassi4333@gmail.com>
2025-10-20 15:10:12 -0300

    punto 1.a tp3 teminado

---
+++ practica/Tp_3/Punto1Main.java
@@ -0,0 +1,42 @@
+package Tp_3;
+
+public class Punto1Main {
+    public static void main(String[] args) {
+        Punto1Pila pila1 = new Punto1Pila();
+
+        // System.out.println(pila1.meter(1));
+        // System.out.println(pila1.meter(2));
+        // System.out.println(pila1.meter(3));
+        // System.out.println(pila1.meter(4));
+        // System.out.println(pila1.meter(5));
+
+        int x = 3;
+        int y = 5;
+        int z = 2;
+
+        pila1.meter(x);
+        pila1.meter(4);
+        x = pila1.sacar();
+        pila1.meter(y);
+        pila1.meter(3);
+        pila1.meter(z);
+        x = pila1.sacar();
+        pila1.meter(2);
+        pila1.meter(x);
+
+
+        while(!pila1.estaVacia()){
+            x = pila1.sacar();
+            System.out.println(x);
+        }
+
+
+
+
+
+        pila1.estaLlena();
+        pila1.estaVacia();
+
+
+    }
+}
